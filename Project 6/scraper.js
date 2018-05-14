9f2868a8377e24c6e398d7c6a3fe8bebb2d5d287
brian walsh <34707134+brianwalsh64@users.noreply.github.com>
2018-05-13 17:56:25 -0700

    Add files via upload

--- Project 6/scraper.js
+++ Project 6/scraper.js
@@ -1,12 +1,13 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const json2csv = require('json2csv');
+const Json2csvParser = require('json2csv').Parser;
+const fields = ['title', 'price', 'imgUrl', 'url', 'time'];
 const fs = require('fs');
 let mainURL = 'http://shirts4mike.com/';
 let entryURL = 'http://shirts4mike.com/shirts.php';
 let links = [];
 let shirtData = [];
-let allShirts = { };
+let allShirts = {};
 
 /* check to see if the folder 'data' is there
 if not, then creat it*/
@@ -27,50 +28,58 @@ let options = {
 };
 rp(options)
   .then( ($) => {
-    $('.products li a').each(function(i, element) {
-      links[i] = {
-        itemLink: $(this).attr('href'),
-      }
-    })
-  console.log(links);
-  })
+      $('.products li a').each(function(i, element) {
+        links[i] = {
+          itemLink: $(this).attr('href'),
+        }// end links
+      })// end each
+      //console.log(links);
+  })// end then
+  .then(() => {scrape()})
+  .then(() => {json2csv()})
   .catch((error) => {
     console.log(error);
   });
-/*So, the code above works fine, but the code below not so much. I dont get any error messages,
-but I dont see my console.log() either. I tested the code below with a static hard coded array
-of links and all works fine. I know this is some asynchronus issue, but i dont know how to fix it.
-I watched the workshop video on Promises and looked at a bunch of stuff, but I am just lost at this
-point. Could someone take a look? */
- /*Now, make another request to the server, actually eight request. Get price, title, imgURL 
- and store in an array of objects, include the  time as well. */
-for(i = 0; i < links.length; i++) {
-    options = {
-      uri: `http://shirts4mike.com/${links[i].itemLink}`,
-      transform: function (body) {
-        return cheerio.load(body);
-      }// end transform
-    };// end options
-  rp(options)
-    .then(($) => {
-      let price = $('.price').text();
-      let title = $('.shirt-details h1').text().slice(4);
-      let imgURL = $('.shirt-picture img').attr('src');
-      let time = new Date().toUTCString();// check other formatts?
-      allShirts = { 
-        price: price,
-        title: title,
-        imgURL: imgURL,
-        time: time,
-      }
-      console.log(allShirts);
-      shirtData.push(allShirts);
-      //console.log(shirtData);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}// end of for loop
-    
 
+ /*Now, make another request to the server, actually eight request. Get price, title,
+ imgURL, url and store in an array of objects, include the  time as well. */
 
+function scrape() {
+  for(i = 0; i < links.length; i++) {
+    let url = `http://shirts4mike.com/${links[i].itemLink}`
+      options = {
+        uri: url,
+        transform: function (body) {
+          return cheerio.load(body);
+        }// end transform
+      };// end options
+    rp(options)
+      .then(($) => {
+        let title = $('.shirt-details h1').text().slice(4);
+        let price = $('.price').text();
+        let imgURL = $('.shirt-picture img').attr('src');
+        let time = new Date().toUTCString();
+        allShirts = { 
+          title: title,
+          price: price,
+          imgURL: imgURL,
+          url: url,
+          time: time,
+        }
+        shirtData.push(allShirts);
+        //console.log(shirtData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }// end of for loop
+}//end scrape
+
+/*Now, convert to and save in a .csv file and store that file the data folder*/
+function json2csv() {
+  const json2csvParser = new Json2csvParser({ fields });
+  const csv = json2csvParser.parse(shirtData);
+  console.log(csv);
+  console.log(allShirts);
+  console.log(shirtData);
+}// end json2csv
